3bad6d7eeceb8446d0e4dc67e5a9e91c9ad3fc0e
Sachin Nadgouda <sachinnadgouda@gmail.com>
2020-05-17 11:06:20 +0200

    F4 added code pipeline foe CICD

---
+++ cdk/config.ts
@@ -0,0 +1,4 @@
+export const githubOwner = 'sachinnadgouda';
+export const repoName = 'my-react-learnings';
+export const gitDevBranch = 'master';
+export const awsSecretsGitHubTokenName = '2a677da15772a92400e5fc6f0c740ecdfeb04587 ';
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -4,49 +4,54 @@ import * as ec2 from '@aws-cdk/aws-ec2';
 import * as ecr from '@aws-cdk/aws-ecr';
 import * as ecs from '@aws-cdk/aws-ecs';
 import * as iam from '@aws-cdk/aws-iam';
-import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
 import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
+import * as codebuild from '@aws-cdk/aws-codebuild';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+import { githubOwner, repoName, awsSecretsGitHubTokenName, gitDevBranch } from '../config'
+import {SecretValue} from "@aws-cdk/core";
 
 export class CdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-	// create dynamo table
-    const table = new dynamodb.Table(this, 'Messages', {
-      partitionKey: {
-        name: 'app_id',
-        type: dynamodb.AttributeType.STRING
-      }, 
-      sortKey: {
-        name: 'created_at',
-        type: dynamodb.AttributeType.NUMBER
-      },
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
-    });
-    new cdk.CfnOutput(this, 'TableName', { value: table.tableName });
-	
-	// create vpc
-	const vpc = new ec2.Vpc(this, "my-react-learnings-cdk-vpc", {
-      cidr: "10.1.0.0/16",
-      maxAzs: 3 // Default is all AZs in region
-    });
-	
-	// create ecr
-	const repository = new ecr.Repository(this, "my-react-learnings-cdk-ecr-api", {
-      repositoryName: "my-react-learnings"
-    });
-	
-	// create ecs cluster
-	const cluster = new ecs.Cluster(this, "my-react-learnings-cluster", {
-      vpc: vpc
-    });
-	
-	// create execution policy iam role
-	const executionRolePolicy =  new iam.PolicyStatement({
-      effect: iam.Effect.ALLOW,
-      resources: ['*'],
-      actions: [
+        // create dynamo table
+        const table = new dynamodb.Table(this, 'Messages', {
+            partitionKey: {
+                name: 'app_id',
+                type: dynamodb.AttributeType.STRING
+            },
+            sortKey: {
+                name: 'created_at',
+                type: dynamodb.AttributeType.NUMBER
+            },
+            billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+            removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
+        });
+        new cdk.CfnOutput(this, 'TableName', {value: table.tableName});
+
+
+        // create vpc
+        const vpc = new ec2.Vpc(this, "my-react-learnings-cdk-vpc", {
+            cidr: "10.1.0.0/16",
+            maxAzs: 3 // Default is all AZs in region
+        });
+
+        // create ecr
+        const repository = new ecr.Repository(this, "my-react-learnings-cdk-ecr-api", {
+            repositoryName: "my-angular-learnings-repo"
+        });
+
+        // create ecs cluster
+        const cluster = new ecs.Cluster(this, "my-react-learnings-cluster", {
+            vpc: vpc
+        });
+
+        // create execution policy iam role
+        const executionRolePolicy = new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            resources: ['*'],
+            actions: [
                 "ecr:GetAuthorizationToken",
                 "ecr:BatchCheckLayerAvailability",
                 "ecr:GetDownloadUrlForLayer",
@@ -54,71 +59,143 @@ export class CdkStack extends cdk.Stack {
                 "logs:CreateLogStream",
                 "logs:PutLogEvents"
             ]
-    });
+        });
 
-	// create task defination
-    const fargateTaskDefinition = new ecs.FargateTaskDefinition(this, 'my-react-learnings-task', {
-      memoryLimitMiB: 512,
-      cpu: 256,
-    });
-    fargateTaskDefinition.addToExecutionRolePolicy(executionRolePolicy);
-    fargateTaskDefinition.addToTaskRolePolicy(new iam.PolicyStatement({
-      effect: iam.Effect.ALLOW,
-      resources: [table.tableArn],
-      actions: ['dynamodb:*']
-    }));
+        // create task defination
+        const fargateTaskDefinition = new ecs.FargateTaskDefinition(this, 'my-react-learnings-task', {
+            memoryLimitMiB: 512,
+            cpu: 256,
+        });
+        fargateTaskDefinition.addToExecutionRolePolicy(executionRolePolicy);
+        fargateTaskDefinition.addToTaskRolePolicy(new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            resources: [table.tableArn],
+            actions: ['dynamodb:*']
+        }));
+        let repoUri = "285661835187.dkr.ecr.us-east-2.amazonaws.com/my-angular-learnings";
+        // create fargate container
+        const container = fargateTaskDefinition.addContainer("my-react-container", {
+            // Use an image from Amazon ECR
 
-	// create fargate container
-    const container = fargateTaskDefinition.addContainer("my-react-container", {
-      // Use an image from Amazon ECR
-      image: ecs.ContainerImage.fromRegistry(repository.repositoryUri),
-      environment: { 
-        'DYNAMODB_MESSAGES_TABLE': table.tableName,
-        'APP_ID' : 'my-react-learnings'
-      }
-      // ... other options here ...
-    });
+            image: ecs.ContainerImage.fromRegistry(repoUri),
+            environment: {
+                'DYNAMODB_MESSAGES_TABLE': table.tableName,
+                'APP_ID': 'my-react-learnings'
+            }
+            // ... other options here ...
+        });
 
-    container.addPortMappings({
-      containerPort: 80
-    });
-	
-	// create fargate service
-	const sg_service = new ec2.SecurityGroup(this, 'my-react-service', { vpc: vpc });
-    sg_service.addIngressRule(ec2.Peer.ipv4('0.0.0.0/0'), ec2.Port.tcp(80));
+        container.addPortMappings({
+            containerPort: 80
+        });
 
-    const service = new ecs.FargateService(this, 'Service', {
-      cluster,
-      taskDefinition: fargateTaskDefinition,
-      desiredCount: 1,
-      assignPublicIp: false,
-      securityGroup: sg_service
-    });
+        // create fargate service
+        const sg_service = new ec2.SecurityGroup(this, 'my-react-service', {vpc: vpc});
+        sg_service.addIngressRule(ec2.Peer.ipv4('0.0.0.0/0'), ec2.Port.tcp(80));
 
-    // Setup AutoScaling policy
-    //const scaling = service.autoScaleTaskCount({ maxCapacity: 2, minCapacity: 1 });
-    //scaling.scaleOnCpuUtilization('CpuScaling', {
-    //  targetUtilizationPercent: 90,
-    //  scaleInCooldown: cdk.Duration.seconds(60),
-    //  scaleOutCooldown: cdk.Duration.seconds(60)
-    // });
-	
-	// create load balancer
-	const lb = new elbv2.ApplicationLoadBalancer(this, 'my-react-learnings-alb', {
-      vpc,
-      internetFacing: true
-    });
+        const service = new ecs.FargateService(this, 'Service', {
+            cluster,
+            taskDefinition: fargateTaskDefinition,
+            desiredCount: 1,
+            assignPublicIp: false,
+            securityGroup: sg_service
+        });
 
-    const listener = lb.addListener('Listener', {
-      port: 80,
-    });
+        // Setup AutoScaling policy
+        //const scaling = service.autoScaleTaskCount({ maxCapacity: 2, minCapacity: 1 });
+        //scaling.scaleOnCpuUtilization('CpuScaling', {
+        //  targetUtilizationPercent: 90,
+        //  scaleInCooldown: cdk.Duration.seconds(60),
+        //  scaleOutCooldown: cdk.Duration.seconds(60)
+        // });
 
-    listener.addTargets('Target', {
-      port: 80,
-      targets: [service],
-      healthCheck: { path: '/' }
-    });
+        // create load balancer
+        const lb = new elbv2.ApplicationLoadBalancer(this, 'my-react-learnings-alb', {
+            vpc,
+            internetFacing: true
+        });
 
-    listener.connections.allowDefaultPortFromAnyIpv4('Open to the world');
-  }
+        const listener = lb.addListener('Listener', {
+            port: 80,
+        });
+
+        listener.addTargets('Target', {
+            port: 80,
+            targets: [service],
+            healthCheck: {path: '/'}
+        });
+
+        listener.connections.allowDefaultPortFromAnyIpv4('Open to the world');
+
+        // create code pipeline
+        const project = new codebuild.PipelineProject(this, 'MyProject', {
+            environment: {
+                buildImage: codebuild.LinuxBuildImage.STANDARD_2_0,
+                privileged: true
+            },
+        });
+
+        // create build role IAM
+        const buildRolePolicy = new iam.PolicyStatement({
+            effect: iam.Effect.ALLOW,
+            resources: ['*'],
+            actions: [
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:GetRepositoryPolicy",
+                "ecr:DescribeRepositories",
+                "ecr:ListImages",
+                "ecr:DescribeImages",
+                "ecr:BatchGetImage",
+                "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
+                "ecr:CompleteLayerUpload",
+                "ecr:PutImage"
+            ]
+        });
+        project.addToRolePolicy(buildRolePolicy);
+
+        const sourceOutput = new codepipeline.Artifact();
+        const buildOutput = new codepipeline.Artifact();
+        const sourceAction = new codepipeline_actions.GitHubSourceAction({
+            actionName: 'GitHub',
+            owner: githubOwner,
+            repo: repoName,
+            oauthToken: new SecretValue(awsSecretsGitHubTokenName),
+            output: sourceOutput,
+            trigger: codepipeline_actions.GitHubTrigger.POLL,
+            branch: gitDevBranch
+        });
+        const buildAction = new codepipeline_actions.CodeBuildAction({
+            actionName: 'CodeBuild',
+            project,
+            input: sourceOutput,
+            outputs: [buildOutput],
+        });
+
+        new codepipeline.Pipeline(this, 'MyPipeline', {
+            stages: [
+                {
+                    stageName: 'Source',
+                    actions: [sourceAction],
+                },
+                {
+                    stageName: 'Build',
+                    actions: [buildAction],
+                },
+                {
+                    stageName: 'Deploy',
+                    actions: [
+                        new codepipeline_actions.EcsDeployAction({
+                                actionName: "ECS-Service",
+                                service: service,
+                                input: buildOutput
+                            }
+                        )
+                    ]
+                }
+            ],
+        });
+    }
 }
